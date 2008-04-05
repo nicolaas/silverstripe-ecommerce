2b641e4cb6c0c695effc3ae99a0335ef5f6b399b
Sean Harvey <sean@silverstripe.com>
2008-04-05 03:29:18 +0000

    Added documentation on what the getCharge method does on ShippingCalculator
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@52169 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/ShippingCalculator.php
+++ code/ShippingCalculator.php
@@ -10,6 +10,15 @@
  */
 abstract class ShippingCalculator extends Object {
 	
+	/**
+	 * Abstract class for calculating and returning a shipping amount
+	 * based on the passed in Order instance. This method is defined on
+	 * the shipping subclasses of ShippingCalculator.
+	 * 
+	 * For an example on how this is created, please see {@link SimpleShippingCalculator}
+	 *
+	 * @param Order $o The order to calculate shipping of
+	 */
 	abstract function getCharge(Order $o);
   
 }
