4a9226a57856ba2e5f8d2bce197fd665f7f47619
Romain Louis <romain@silverstripe.com>
2008-04-16 00:49:23 +0000

    static function adding to able the user to manage himself the creation of the order modifier
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@52806 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/OrderModifier.php
+++ code/OrderModifier.php
@@ -23,9 +23,17 @@ class OrderModifier extends DataObject {
 	
 	private static $isChargable = true;
 	
-	public function __construct($order = null) {
+	public function __construct(Order $order = null) {
 		parent::__construct();
 		$this->order = $order;
+	}
+	
+	/*
+	 * This function is called when the order inits its modifiers.
+	 * It is better than directly construct the modifier in the Order class because, the user may need to create several modifiers or customize it.
+	 */
+	public static function init_for_order($className, Order $order) {
+		$order->addModifier(new $className($order));
 	}
 	
 	/*
@@ -39,6 +47,13 @@ class OrderModifier extends DataObject {
 	function updateOrderInformationEditableFields(FieldSet &$fields) {
 	}
 	
+	// Functions called from the Cart
+	function ShowInCart() {return true;}
+	function TitleForCart() {return 'Modifier';}
+	function ValueIdForCart() {return 'Cart_' . $this->ValueIdForTable();}
+	function ValueForCart() {return $this->ValueForTable();}
+	
+	// Functions called from the Order table
 	function ShowInOrderTable() {return true;}
 	function ClassNameForTable() {
 		if($this->ID) return $this->ClassName;
