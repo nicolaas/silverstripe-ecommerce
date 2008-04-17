b3d0c92c96b7357a2e0aea755fde01b9e90f34c9
Romain Louis <romain@silverstripe.com>
2008-04-17 00:45:56 +0000

    Live functions adding
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@52899 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/SimpleShippingModifier.php
+++ code/SimpleShippingModifier.php
@@ -11,6 +11,12 @@
  */
 class SimpleShippingModifier extends OrderModifier {
 	
+	static $db = array(
+		'Country' => 'Text',
+		'CountryCode' => 'Text',
+		'ShippingChargeType' => "Enum(array('Default','ForCountry'))"
+	);
+	
 	static $default_charge = 0;
 	static $charges_by_country = array();
 
@@ -30,14 +36,33 @@ class SimpleShippingModifier extends OrderModifier {
 		self::$charges_by_country = array_merge(self::$charges_by_country, $countryMap);
 	}
 	
+	//1) Attributes Functions Access
+	
+	function Country() {return $this->ID ? $this->Country : $this->LiveCountry();}
+	function CountryCode() {return $this->ID ? $this->CountryCode : $this->LiveCountryCode();}
+	function IsDefaultCharge() {return $this->ID ? $this->ShippingChargeType == 'Default' : $this->LiveIsDefaultCharge();}
+	
+	function LiveCountry($code = false) {
+		$order = $this->Order();
+		return $order->findShippingCountry($code);
+	}
+	function LiveCountryCode() {return $this->LiveCountry(true);}
+	function LiveIsDefaultCharge() {return ! ($this->LiveCountryCode() && array_key_exists($this->LiveCountryCode(), self::$charges_by_country));}
+	
+	/**
+	 * Find the amount for the shipping on the shipping country for the order.
+	 */
+	function LiveAmount() {return $this->LiveIsDefaultCharge() ? self::$default_charge : self::$charges_by_country[$this->LiveCountryCode()];}
+	
+	//2) Display Functions
+	
 	// Functions called from the Cart
 	function ShowInCart() {return $this->getValue() > 0;}
 	function TitleForCart() {return 'Shipping';}
 	
 	// Functions called from the Order table
 	function TitleForTable() {
-		$order = $this->getOrder();
-		if($shippingCountry = $order->findShippingCountry()) return "Shipping to $shippingCountry";
+		if($shippingCountry = $this->Country()) return "Shipping to $shippingCountry";
 		else return 'Shipping';
 	}
 	function ValueIdForTable() {return 'ShippingCost';}
@@ -47,12 +72,13 @@ class SimpleShippingModifier extends OrderModifier {
 		return $val->Nice();
 	}
 	
-	/**
-	 * Find the amount for the shipping on the shipping country for the order.
-	 */
-	function getLiveAmount() {
-		$order = $this->getOrder();
-		if($order->findShippingCountry(true) && array_key_exists($order->findShippingCountry(true), self::$charges_by_country)) return self::$charges_by_country[$order->findShippingCountry(true)];
-		else return self::$default_charge;
+	//3) Database Writing Functions
+	
+	public function write() {
+		$this->Country = $this->Country();
+		$this->CountryCode = $this->CountryCode();
+		$this->ShippingChargeType = $this->IsDefaultCharge() ? 'Default' : 'ForCountry';
+		parent::write();
 	}
+	
 }
