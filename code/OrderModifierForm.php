71fe61c48a62fbdde2d21ff08c2aefbea2bbc2d5
Romain Louis <romain@silverstripe.com>
2008-04-28 23:33:15 +0000

    OrderModifierForm adding
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@53641 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ code/OrderModifierForm.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package ecommerce
+ */
+ 
+/** 
+ * The OrderModifier class is a databound object for handling the additionale charges or deductions of an order.
+ */
+ 
+class OrderModifierForm extends Form {
+	/*
+	protected $order;
+	
+	function __construct(Order $order, CheckoutPage $checkoutPage, $name, FieldSet $fields, FieldSet $actions, $validator = null) {
+		$this->order = $order;
+		
+		parent::__construct($checkoutPage, $name, $fields, $actions, $validator);
+	}
+	*/
+}
+
+?>
