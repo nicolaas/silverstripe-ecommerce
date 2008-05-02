a485ee53ab327ea669ed5a57843a92932ee460ed
Romain Louis <romain@silverstripe.com>
2008-05-02 05:53:18 +0000

    Message content changing
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@53910 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/MemberForm.php
+++ code/MemberForm.php
@@ -14,7 +14,7 @@ class MemberForm extends Form {
 		$member = Member::currentUser();
 		
 		$contactFields = $member->getEcommerceFields();
-		$logoutField = new LiteralField('LogoutNote', "<p class=\"message good\">" . _t("MemberForm.LOGGEDIN","You are currently logged in.") . "Click <a href=\"Security/logout\" title=\"Click here to log out\">here</a> to log out.</p>");
+		$logoutField = new LiteralField('LogoutNote', "<p class=\"message good\">" . _t("MemberForm.LOGGEDIN","You are currently logged in as ") . $member->FirstName . ' ' . $member->Surname . ". Click <a href=\"Security/logout\" title=\"Click here to log out\">here</a> to log out.</p>");
 		$passwordField = new ConfirmedPasswordField("Password", "Password");
 		if($member && $member->Password != '') {
 			$passwordField->setCanBeEmpty(true);
