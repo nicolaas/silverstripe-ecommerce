b330d0844ff5abcccc8cd9b42dbd5fcf30dee751
Romain Louis <romain@silverstripe.com>
2008-05-22 22:38:25 +0000

    Payment reorganisation
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@54993 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/payments/PayPalPayment.php
+++ code/payments/PayPalPayment.php
@@ -18,7 +18,7 @@ class PayPalPayment_Handler extends Controller {
 	 * Only For PayPal type payment, for dealing with reply from PayPal
 	 */
 	function paid(){
-		$order = DataObject::get_by_id("Order", $_REQUEST[invoice]);
+		$order = DataObject::get_by_id('Order', $_REQUEST[invoice]);
 			
 		if($_REQUEST['payment_status']=='Completed'){
 			
@@ -31,7 +31,7 @@ class PayPalPayment_Handler extends Controller {
 			//$payment->GST = $_REQUEST[tax];
 			$payment->OrderID = $order->ID;
 			$payment->Status = 'Success';
-			$payment->Currentcy = $_REQUEST[mc_currency];
+			$payment->Currency = $_REQUEST[mc_currency];
 			$payment->PaymentMethod = 'PayPal';
 			$payment->DpsTxnRef = $_REQUEST[txn_id];
 			
