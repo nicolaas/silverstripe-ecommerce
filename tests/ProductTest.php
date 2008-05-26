9116c50f343ad03fa00c1df9759c63b4be4ea89c
Sam Minnee <sam@silverstripe.com>
2008-05-26 06:24:15 +0000

    Removed junk output from ProductTest
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@55137 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -6,12 +6,12 @@
 class ProductTest extends FunctionalTest {
 	static $fixture_file = 'ecommerce/tests/ecommerce.yml';
 	static $disable_theme = true;
+	static $use_draft_site = true;
 
 	function testPage() {
-		$this->useDraftSite();
-
-		$_REQUEST['showqueries'] = 1;
+		/*
 		Debug::show($this->get('group-1'));
+		*/
 	}
 
 }
