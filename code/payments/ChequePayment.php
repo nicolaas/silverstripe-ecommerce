8afbbbdf25b952785e0fe1f750fafe963cefe460
Romain Louis <romain@silverstripe.com>
2008-05-29 03:19:56 +0000

    new order/payment status management setting and code cleaning
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@55316 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/payments/ChequePayment.php
+++ code/payments/ChequePayment.php
@@ -8,22 +8,16 @@
  * Payment object representing a cheque payment
  */
 class ChequePayment extends Payment {
+	
 	/**
 	 * Process the Cheque payment method
 	 */
 	function processPayment($data, $form) {
-		if(!$this->PaymentMethod)
-			$this->PaymentMethod = "Cheque";
-			
-		if(!$this->Status)
-			$this->Status = "Pending";
-			
-		if(!$this->Message)
-			$this->Message = "<p class=\"warningMessage\">"._t("ChequePayment.MESSAGE","Payment accepted via Cheque. Please note: products will not be shipped until payment has been received.")."</p>";
+		$this->Status = 'Pending';
+		$this->Message = '<p class="warningMessage">' . _t('ChequePayment.MESSAGE', 'Payment accepted via Cheque. Please note : products will not be shipped until payment has been received.') . '</p>';
 		
-		$result[Payment::$success] = true;
-		$result['PaymentID'] = $this->write();
-		return $result;
+		$this->write();
+		return new Payment_Success();
 	}
 	
 	function getPaymentFormFields() {
@@ -33,23 +27,12 @@ class ChequePayment extends Payment {
 			new HiddenField("Cheque", "Cheque", 0)
 		);
 	}
-	function getPaymentFormRequirements() {
-		return null;
-	}
+	function getPaymentFormRequirements() {return null;}
 
 	/**
 	 * Returns the Cheque content from the CheckoutPage
 	 */
-	function ChequeContent() {
-		return DataObject::get_one('CheckoutPage')->ChequeMessage;
-	}
-	
-	/**
-	 *  Function used for in template to check if payment is cheque.
-	 */
-	function IsCheque(){
-		return true;
-	}
+	function ChequeContent() {return DataObject::get_one('CheckoutPage')->ChequeMessage;}
 	
 }
 
