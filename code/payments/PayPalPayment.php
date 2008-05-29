d6cb7d7ba80a65a42501e16d1ee4f68007959bba
Romain Louis <romain@silverstripe.com>
2008-05-29 03:16:00 +0000

    new order/payment status management setting and credit cards options adding
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@55311 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/payments/PayPalPayment.php
+++ code/payments/PayPalPayment.php
@@ -1,9 +1,10 @@
 <?php
 
+
 /**
  * @package ecommerce
  */
- 
+
 /**
  * Sub-class of Payment that supports PayPal Website Payment Standard 
  * (https://www.paypal.com/IntegrationCenter/ic_standard_home.html) as its payment processor
@@ -36,130 +37,160 @@
   have a PayPalPaymentPage.ss template or replace some code below (search for renderwith)
   on the PayPalPaymentPage you can use $PayPalInstructions (see above)
   
-**/ 
+**/
 
 class PayPalPayment extends Payment {
 	
-	// URLs
+	// PayPal Informations
 	
+	protected static $privacy_link = 'https://www.paypal.com/us/cgi-bin/webscr?cmd=p/gen/ua/policy_privacy-outside';
+	protected static $logo = 'ecommerce/images/payments/paypal.jpg';
+	
+	// URLs
+
 	protected static $url = 'https://www.paypal.com/cgi-bin/webscr';
 	protected static $test_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
-	
+
 	// Test Mode
-	
+
 	protected static $test_mode = false;
 	protected static $test_account_email;
 	static function set_test_mode($test_account_email) {
-		self::$test_mode = true;
-		self::$test_account_email = $test_account_email;
+		self :: $test_mode = true;
+		self :: $test_account_email = $test_account_email;
 	}
-	
+
 	// Payment Informations
-	
+
 	protected static $account_email;
-	static function set_account_email($account_email) {self::$account_email = $account_email;}
-	
+	static function set_account_email($account_email) {
+		self :: $account_email = $account_email;
+	}
+
 	// PayPal Pages Style Optional Informations
-	
+
 	protected static $continue_button_text;
-	static function set_continue_button_text($continue_button_text) {self::$continue_button_text = $continue_button_text;}
-	
+	static function set_continue_button_text($continue_button_text) {
+		self :: $continue_button_text = $continue_button_text;
+	}
+
 	protected static $header_image_url;
-	static function set_header_image_url($header_image_url) {self::$header_image_url = $header_image_url;}
-	
+	static function set_header_image_url($header_image_url) {
+		self :: $header_image_url = $header_image_url;
+	}
+
 	protected static $header_back_color;
-	static function set_header_back_color($header_back_color) {self::$header_back_color = $header_back_color;}
-	
+	static function set_header_back_color($header_back_color) {
+		self :: $header_back_color = $header_back_color;
+	}
+
 	protected static $header_border_color;
-	static function set_header_border_color($header_border_color) {self::$header_border_color = $header_border_color;}
-	
+	static function set_header_border_color($header_border_color) {
+		self :: $header_border_color = $header_border_color;
+	}
+
 	protected static $payflow_color;
-	static function set_payflow_color($payflow_color) {self::$payflow_color = $payflow_color;}
-	
+	static function set_payflow_color($payflow_color) {
+		self :: $payflow_color = $payflow_color;
+	}
+
 	protected static $back_color;
-	static function set_back_color_black() {self::$back_color = '1';}
-	
+	static function set_back_color_black() {self :: $back_color = '1';}
+
 	protected static $image_url;
-	static function set_image_url($image_url) {self::$image_url = $image_url;}
-	
+	static function set_image_url($image_url) {
+		self :: $image_url = $image_url;
+	}
+
 	protected static $page_style;
-	static function set_page_style($page_style) {self::$page_style = $page_style;}
+	static function set_page_style($page_style) {
+		self :: $page_style = $page_style;
+	}
 	
 	function getPaymentFormFields() {
+		$logo = '<img src="' . self::$logo . '" alt="Credit card payments powered by PayPal"/>';
+		$privacyLink = '<a href="' . self::$privacy_link . '" target="_blank" title="Read PayPal\'s privacy policy">' . $logo . '</a><br/>';
 		return new FieldSet(
-			new LiteralField('PayPalInfo', '<a href="https://www.paypal.com/us/cgi-bin/webscr?cmd=p/gen/ua/policy_privacy-outside" title="Read PayPal\'s privacy policy" target="_blank"><img src="http://farm2.static.flickr.com/1317/1365689127_5567060b2d.jpg" alt="Payments powered by PayPal"/></a>')
+			new LiteralField('PayPalInfo', $privacyLink),
+			new LiteralField(
+				'PayPalPaymentsList',
+				'<img src="ecommerce/images/payments/methods/visa.jpg" alt="Visa"/>' .
+				'<img src="ecommerce/images/payments/methods/mastercard.jpg" alt="MasterCard"/>' .
+				'<img src="ecommerce/images/payments/methods/american-express.gif" alt="American Express"/>' .
+				'<img src="ecommerce/images/payments/methods/discover.jpg" alt="Discover"/>' .
+				'<img src="ecommerce/images/payments/methods/paypal.jpg" alt="PayPal"/>'
+			)
 		);
 	}
-	
+
 	function getPaymentFormRequirements() {return null;}
-	
+
 	function processPayment($data, $form) {
 		$page = new Page();
-		
-		$page->URLSegment = 'paypal';
+
 		$page->Title = 'Redirection to PayPal...';
-		$page->Logo = '<img src="http://farm2.static.flickr.com/1317/1365689127_5567060b2d.jpg" alt="Payments powered by PayPal"/>';
+		$page->Logo = '<img src="' . self::$logo . '" alt="Payments powered by PayPal"/>';
 		$page->Form = $this->PayPalForm();
 
 		$controller = new Page_Controller($page);
-		
+
 		$form = $controller->renderWith('PaymentProcessingPage');
-		
+
 		return new Payment_Processing($form);
 	}
-	
+
 	function PayPalForm() {
-	 	
-	 	// 1) Main Informations
-	 	
-	 	$order = $this->Order();
-	 	$items = $order->Items();
-	 	$member = $this->Member();
-	 	
-	 	// 2) Main Settings
-	 	
-	 	$url = self::$test_mode ? self::$test_url : self::$url;
-	 	$inputs['cmd'] = '_cart';
-	 	$inputs['upload'] = '1';
-	 	
-	 	// 3) Items Informations
-	 	
-	 	$cpt = 0;
-		foreach($items as $item) {
-			$inputs['item_name_' . ++$cpt] = $item->Title();
+
+		// 1) Main Informations
+
+		$order = $this->Order();
+		$items = $order->Items();
+		$member = $this->Member();
+
+		// 2) Main Settings
+
+		$url = self :: $test_mode ? self :: $test_url : self :: $url;
+		$inputs['cmd'] = '_cart';
+		$inputs['upload'] = '1';
+
+		// 3) Items Informations
+
+		$cpt = 0;
+		foreach ($items as $item) {
+			$inputs['item_name_' . ++ $cpt] = $item->Title();
 			// item_number is unnecessary
 			$inputs['amount_' . $cpt] = $item->UnitPrice();
 			$inputs['quantity_' . $cpt] = $item->Quantity;
 		}
-		
+
 		// 4) Payment Informations
-		
-		$inputs['business'] = self::$test_mode ? self::$test_account_email : self::$account_email;
+
+		$inputs['business'] = self :: $test_mode ? self :: $test_account_email : self :: $account_email;
 		$inputs['custom'] = $this->ID;
 		// Add Here The Shipping And/Or Taxes
 		$inputs['currency_code'] = $this->Currency;
-		
+
 		// 5) Redirection Informations
-		
-		$inputs['cancel_return'] = Director::absoluteBaseURL() . PayPalPayment_Handler::cancel_link();
-		$inputs['return'] = Director::absoluteBaseURL() . PayPalPayment_Handler::complete_link();
+
+		$inputs['cancel_return'] = Director :: absoluteBaseURL() . PayPalPayment_Handler :: cancel_link();
+		$inputs['return'] = Director :: absoluteBaseURL() . PayPalPayment_Handler :: complete_link();
 		$inputs['rm'] = '2';
 		// Add Here The Notify URL
-		
+
 		// 6) PayPal Pages Style Optional Informations
-		
-		if(self::$continue_button_text) $inputs['cbt'] = self::$continue_button_text;
-		
-		if(self::$header_image_url) $inputs['cpp_header_image'] = urlencode(self::$header_image_url);
-		if(self::$header_back_color) $inputs['cpp_headerback_color'] = self::$header_back_color;
-		if(self::$header_border_color) $inputs['cpp_headerborder_color'] = self::$header_border_color;
-		if(self::$payflow_color) $inputs['cpp_payflow_color'] = self::$payflow_color;
-		if(self::$back_color) $inputs['cs'] = self::$back_color;
-		if(self::$image_url) $inputs['image_url'] = urlencode(self::$image_url);
-		if(self::$page_style) $inputs['page_style'] = self::$page_style;
-		
+
+		if (self :: $continue_button_text) $inputs['cbt'] = self :: $continue_button_text;
+
+		if (self :: $header_image_url) $inputs['cpp_header_image'] = urlencode(self :: $header_image_url);
+		if (self :: $header_back_color)	$inputs['cpp_headerback_color'] = self :: $header_back_color;
+		if (self :: $header_border_color) $inputs['cpp_headerborder_color'] = self :: $header_border_color;
+		if (self :: $payflow_color) $inputs['cpp_payflow_color'] = self :: $payflow_color;
+		if (self :: $back_color) $inputs['cs'] = self :: $back_color;
+		if (self :: $image_url) $inputs['image_url'] = urlencode(self :: $image_url);
+		if (self :: $page_style) $inputs['page_style'] = self :: $page_style;
+
 		// 7) Prepopulating Customer Informations
-		
+
 		$inputs['first_name'] = $member->FirstName;
 		$inputs['last_name'] = $member->Surname;
 		$inputs['address1'] = $member->Address;
@@ -167,15 +198,16 @@ class PayPalPayment extends Payment {
 		$inputs['city'] = $member->City;
 		$inputs['country'] = $member->Country;
 		$inputs['email'] = $member->Email;
-		
-		if($member->hasMethod('getState')) $inputs['state'] = $member->getState();
-		if($member->hasMethod('getZip')) $inputs['zip'] = $member->getZip();
- 		
- 		// 8) Form Creation
- 		
-	 	foreach($inputs as $name => $value) $fields .= '<input type="hidden" name="' . $name . '" value="' . $value . '"/>';
-	 	
-	 	return <<<HTML
+
+		if ($member->hasMethod('getState'))	$inputs['state'] = $member->getState();
+		if ($member->hasMethod('getZip')) $inputs['zip'] = $member->getZip();
+
+		// 8) Form Creation
+
+		foreach ($inputs as $name => $value)
+			$fields .= '<input type="hidden" name="' . $name . '" value="' . $value . '"/>';
+
+		return<<<HTML
 			<form id="PaymentForm" method="post" action="$url">$fields</form>
 			<script type="text/javascript">$('PaymentForm').submit();</script>
 			<!-- script type="text/javascript">
@@ -191,49 +223,41 @@ HTML;
  * Handler for responses from the PayPal site
  */
 class PayPalPayment_Handler extends Controller {
-	
+
 	static $URLSegment = 'paypal';
-	
-	static function cancel_link() {return self::$URLSegment . '/cancel';}
-	static function complete_link() {return self::$URLSegment . '/complete';}
-		
+
+	static function cancel_link() {
+		return self :: $URLSegment . '/cancel';
+	}
+	static function complete_link() {
+		return self :: $URLSegment . '/complete';
+	}
+
 	/**
 	 * Only For PayPal type payment, for dealing with reply from PayPal
 	 */
 	function complete() {
-		$payment = DataObject::get_by_id('PayPalPayment', $_REQUEST['custom']);
-		$order = $payment->Order();
-		if($_REQUEST['payment_status'] == 'Completed') {
+		$payment = DataObject :: get_by_id('PayPalPayment', $_REQUEST['custom']);
+		if ($_REQUEST['payment_status'] == 'Completed') {
 			$payment->Status = 'Success';
-			$payment->Currency = $_REQUEST['mc_currency'];
+			//$payment->Currency = $_REQUEST['mc_currency'];
 			$payment->TxnRef = $_REQUEST['txn_id'];
-			$order->Status = 'Paid';
-			$order->sendReceipt();
 		}
-		else {
-			$payment->Status = 'Failure';
-			$order->Status = 'Unpaid';
-		}
-  		  		
-  		$payment->write();
-  		$order->write();
-  		
-  		Director::redirect($order->Link());
-		return;
-	}
-		
-	function cancel() {
-		$payment = DataObject::get_by_id('PayPalPayment', $_REQUEST['custom']);
-		$order = $payment->Order();
-		
-		$payment->Status = 'Failure';
-		$order->Status = 'Unpaid';
+		else $payment->Status = 'Failure';
 		
 		$payment->write();
-  		$order->write();
-  		
-  		Director::redirect($order->Link());
-		return;
+		
+		$payment->redirectToOrder();
+	}
+
+	function cancel() {
+		$payment = DataObject :: get_by_id('PayPalPayment', $_REQUEST['custom']);
+		
+		$payment->Status = 'Failure';
+		
+		$payment->write();
+		
+		$payment->redirectToOrder();
 	}
 }
 
@@ -266,5 +290,4 @@ class PayPalPayment_Handler extends Controller {
  mc_gross=70.00
  custom=39
 */
-
 ?>
