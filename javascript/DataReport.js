e32c92741fe63bdce4810a0ff11d45302651c570
Sean Harvey <sean@silverstripe.com>
2008-09-12 04:58:06 +0000

    API CHANGE Moved DataReport and SQLReport into the ecommerce module, since this is the only place it is used, plus it's going to be deprecated soon anyway
    BUGFIX Removed DataReport.js calls in LeftAndMain until we figure out a better way of doing
    
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@62325 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/DataReport.js
+++ javascript/DataReport.js
@@ -11,6 +11,23 @@ DataReport.prototype = {
 			},
 			'#Go' : {
 				onclick: this.filterOnChange.bind(this)
+			},
+			'input.DataReport_ExportToCSVButton' : {
+				onclick: function() {
+					var pageID = $('Form_EditForm_ID').value;
+					var idParts = this.id.split('_');
+					var formName = idParts[idParts.length-2];
+				
+					var reportType = $('DataReport_Type').value;
+					var url = baseHref() + 'admin/ReportField_Controller/exporttocsv/' + pageID + '/' + reportType + '/' + formName + '.csv';
+					
+					//alert( url );
+					//var exportWindow = window.open( url, '_self' );
+					//alert( exportWindow.location );
+					window.location.href = url;
+					
+					return false;
+				}
 			}
 		});
 	},
