de1dd1818e5b5b3d984365968d7c79b629d781ce
Normann Lou <normann@silverstripe.com>
2009-03-17 06:21:03 +0000

    disable two deperated functions in _config.php file of the module.
    git-svn-id: http://svn.silverstripe.com/open/modules/ecommerce/trunk@73215 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- _config.php
+++ _config.php
@@ -13,16 +13,26 @@ Director::addRules(50, array(
 	PayPalPayment_Handler::$URLSegment . '/$Action/$ID' => 'PayPalPayment_Handler'
 ));
 
+/*
+ * MemberTableField::addPermissions() has been deperated,
+ * Please set permissions using setPermissions(Array) on the MemberTableField object.
+ */
+//MemberTableField::addPermissions(array('show', 'export'));
+
+/*
+ * MemberTableField::addMembershipFields() has been deprecated.
+ * Please implement updateSummaryFields() on a Member decorator instead.
+ */
+
 // Add additional fields to the MemberTableField in the CMS for e-commerce.
-MemberTableField::addPermissions(array('show', 'export'));
-MemberTableField::addMembershipFields(array(
+/*MemberTableField::addMembershipFields(array(
 	'Address' => 'Address',
 	'AddressLine2' => 'Address Line 2',
 	'HomePhone' => 'Home Phone',
 	'MobilePhone' => 'Mobile Phone',
 	'City' => 'City',
 	'Country' => 'Country'
-));
+));*/
 
 LeftAndMain::require_css('ecommerce/css/DataReportCMSMain.css');
 LeftAndMain::require_javascript('ecommerce/javascript/DataReport.js');
